aa45ad252d07377ca1c3b562903fe85d440ce68d
zt <zhoutao@ydmob.com>
2024-01-31 17:15:52 +0800

    0131

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,13 @@
  * @Author: zt zhoutao@ydmob.com
  * @Date: 2023-12-15 11:26:15
  * @LastEditors: zt zhoutao@ydmob.com
- * @LastEditTime: 2024-01-31 15:02:16
+ * @LastEditTime: 2024-01-31 16:33:40
  * @FilePath: /client/src/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import ReactDOM from "react-dom/client";
 import "./index.css";
+// import 'antd/dist/antd'
 import App from "./App";
 // import { MovieServices } from './services/MovieServices';
 const root = ReactDOM.createRoot(
@@ -16,3 +17,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <App />
 );
+
+
+// 界面
+// antd：UI库
--- client/src/pages/Movie/Layout/index.tsx
+++ client/src/pages/Movie/Layout/index.tsx
@@ -2,35 +2,52 @@
  * @Author: zt zhoutao@ydmob.com
  * @Date: 2024-01-31 12:07:16
  * @LastEditors: zt zhoutao@ydmob.com
- * @LastEditTime: 2024-01-31 15:12:39
+ * @LastEditTime: 2024-01-31 17:11:12
  * @FilePath: /client/src/pages/Movie/Layout/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React from "react";
-import { NavLink, Route } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 import Home from "../Home";
 import MovieList from "../MovieList";
 import AddMovie from "../AddMovie";
 import EditMovie from "../EditMovie";
+import { Layout, Menu } from 'antd'
+import './index.css'
+const { Header, Sider, Content } = Layout;
 
-const Layout: React.FC = function () {
-    return (<div>
-        <header>
-            <ul>
-                <li><NavLink to='/'>首页</NavLink></li>
-                <li><NavLink to='/movie'>电影列表</NavLink></li>
-                <li><NavLink to='/movie/add'>添加电影</NavLink></li>
-                <li><NavLink to='/movie/edit/test'>修改电影</NavLink></li>
-            </ul>
-            <div>
-                <Route path="/" component={Home} exact={true}></Route>
-                <Route path="/movie" component={MovieList} exact={true}></Route>
-                <Route path="/movie/add" component={AddMovie}></Route>
-                <Route path="/movie/edit/:id" component={EditMovie}></Route>
-            </div>
-        </header>
+const _Layout: React.FC = function () {
+    return (<div className="layout-container">
+        <Layout>
+            <Header className="header">
+                <NavLink to='/'>MovieSys</NavLink>
+            </Header>
+            <Layout>
+                <Sider theme="light">
+                    <Menu defaultSelectedKeys={['1']} mode="inline" theme="light">
+                        <Menu.Item key={'1'}>
+                            <NavLink to='/'>首页</NavLink>
+                        </Menu.Item>
+                        <Menu.Item key={'2'}>
+                            <NavLink to='/movie'>电影列表</NavLink>
+                        </Menu.Item>
+                        <Menu.Item key={'3'}>
+                            <NavLink to='/movie/add'>电影添加</NavLink>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Content>
+                    <div className="main">
+                        <Route path="/" component={Home} exact={true}></Route>
+                        <Route path="/movie" component={MovieList} exact={true}></Route>
+                        <Route path="/movie/add" component={AddMovie}></Route>
+                        <Route path="/movie/edit/:id" component={EditMovie}></Route>
+                    </div>
+                </Content>
+            </Layout>
+        </Layout>
     </div>)
 }
 
 
-export default Layout
+export default _Layout
